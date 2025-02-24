055f1b467814471422bcdd03c19128e35e461839
Shreyas Patange <115157819+IWhitebird@users.noreply.github.com>
2025-02-25 02:22:21 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -31,7 +31,7 @@ declare module 'next-auth/jwt' {
 
 
 export const authOptions: AuthOptions = {
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
     maxAge: 30 * 24 * 60 * 60, // 30 days
