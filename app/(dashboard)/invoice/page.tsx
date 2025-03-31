d684eb726b5f009abbb0eb6072836676de50d8ae
Shreyas Patange <115157819+IWhitebird@users.noreply.github.com>
2025-03-31 19:38:55 +0530

    invoice ui just like  others

--- app/(dashboard)/invoice/page.tsx
+++ app/(dashboard)/invoice/page.tsx
@@ -207,12 +207,9 @@ export default function InvoicePage() {
     <div className="space-y-4 overflow-hidden max-w-[100vw] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
       <Card>
         <CardHeader>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
           <CardTitle>Invoices</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="space-y-4">
-            
-            <div className="flex flex-col sm:flex-row gap-2">
+          <div className="flex flex-col sm:flex-row gap-2">
               <div className="w-full sm:max-w-[300px]">
                 <Input
                   type="text"
@@ -231,6 +228,31 @@ export default function InvoicePage() {
                 >Clear Date</Button>
               </div>
             </div>
+          </div>
+
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            
+            {/* <div className="flex flex-col sm:flex-row gap-2">
+              <div className="w-full sm:max-w-[300px]">
+                <Input
+                  type="text"
+                  placeholder="Search invoice number..."
+                  value={invoiceSearchTerm}
+                  onChange={(e) => setInvoiceSearchTerm(e.target.value)}
+                  className="w-full"
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <DatePicker date={selectedDate} setDate={setSelectedDate} />
+                <Button
+                  variant={'outline'}
+                  disabled={!selectedDate || moment(selectedDate).isSame(moment(), 'day')}
+                  onClick={() => setSelectedDate(moment().startOf('day').toDate())}
+                >Clear Date</Button>
+              </div>
+            </div> */}
 
             <div className="overflow-x-auto w-full border rounded-lg m-auto max-w-[100vw] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
               <Table className=''>
